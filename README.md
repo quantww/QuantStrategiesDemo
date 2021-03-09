275e0ed0a5fe94e2e895c1c75d614db92a2e9c2e
guang.xie <guang.xie@ucloud.cn>
2021-03-09 20:31:49 +0800

    feat: init project

--- README.md
+++
@@ -1,3 +0,0 @@
-# 基于 `BaseOnThenextquant` 框架的一些交易策略小demo
-
-# @links: https://thenextquant.com/docs/overview/
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def initialize():
+    from strategy.binance_strategy import BinanceStrategy
+    BinanceStrategy()
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
